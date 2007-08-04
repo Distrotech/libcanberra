fa49d80fd0bd59b54dd97351057f2b9bb9db60cc
Lennart Poettering <lennart@poettering.net>
2007-08-04 15:50:20 +0000

    update
    git-svn-id: file:///home/lennart/svn/public/libcanberra/trunk@4 01b60673-d06a-42c0-afdd-89cb8e0f78ac

--- canberra.h
+++ canberra.h
@@ -6,17 +6,19 @@ typedef struct cbr_context cbr_context_t;
 typedef enum {
     CBR_META_NULL = -1,
     CBR_META_NAME = 0,
-    CBR_META_SOUND_FILE_WAV,
-    CBR_META_SOUND_FILE_OGG,
-    CBR_META_DESCRIPTION,
-    CBR_META_VOLUME,
+    CBR_META_IDENTIFIER,
     CBR_META_CLIENT_NAME,
+    CBR_META_CLIENT_IDENTIFIER,
+    CBR_META_SOUND_FILE,
+    CBR_META_VOLUME,
     CBR_META_ROLE,
     CBR_META_X11_DISPLAY,
     CBR_META_X11_WINDOW,
     CBR_META_LANGUAGE,
     CBR_META_ICON_NAME,
-    CBR_META_ICON_PNG,
+    CBR_META_ICON_FILE,
+    CBR_META_PRIORITY,
+
     _CBR_META_MAX,
 } cbr_meta_t;
 
@@ -26,6 +28,7 @@ int cbr_context_set(cbr_context_t *c, ...);
 int cbr_context_set_arbitrary(cbr_context_t *c, cbr_meta_t m, const void *c, size_t len);
 int cbr_context_play(cbr_context_t *c, int id, ...);
 int cbr_context_cancel(cbr_context_t *c, int id);
+int cbr_context_cache(cbr_context_t *c, ...);
 
 
 int main(int argc, char *argv[]) {
@@ -55,7 +58,7 @@ int main(int argc, char *argv[]) {
      * data set in any previous cbr_context_set() calls. */
     cbr_context_play(c, id,
                      CBR_META_NAME, "click-event",
-                     CBR_META_SOUND_FILE_WAV, "/usr/share/sounds/foo.wav",
+                     CBR_META_SOUND_FILE, "/usr/share/sounds/foo.wav",
                      CBR_META_DESCRIPTION, "Button has been clicked",
                      CBR_META_ICON_NAME, "clicked",
                      -1);
