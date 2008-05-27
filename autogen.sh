5a98e421a0a00c55431e155bcc8eb71c0d4aa76c
Lennart Poettering <lennart@poettering.net>
2008-05-27 11:35:04 +0000

    add autogen.sh to make Marc-Andre happy
    git-svn-id: file:///home/lennart/svn/public/libcanberra/trunk@16 01b60673-d06a-42c0-afdd-89cb8e0f78ac

---
+++ autogen.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# $Id$
+#
+# This file is part of libcanberra.
+#
+# Copyright 2008 Lennart Poettering
+#
+# libcanberra is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 2.1 of the
+# License, or (at your option) any later version.
+#
+# libcanberra is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with libcanberra. If not, If not, see
+# <http://www.gnu.org/licenses/>.
+
+# Only there to make jhbuild and Marc-Andre happy
+
+NOCONFIGURE=1 ./bootstrap.sh
+
+exec ./configure  "$@"
