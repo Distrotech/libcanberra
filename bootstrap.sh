6d682ecd483add959fab108847a752ff39ec0766
Lennart Poettering <lennart@poettering.net>
2008-08-06 00:05:09 +0200

    fix license wording

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@
 # Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public
-# License along with libcanberra. If not, If not, see
+# License along with libcanberra. If not, see
 # <http://www.gnu.org/licenses/>.
 
 VERSION=1.9
